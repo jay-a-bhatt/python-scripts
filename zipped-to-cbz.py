1d8324d01f14497c428487c97d79648fabce9391
Jay Bhatt <bbbaj2404@gmail.com>
2025-01-01 13:31:44 -0700

    Add .rar support to zipped-to-cbz.py

--- zipped-to-cbz.py
+++ zipped-to-cbz.py
@@ -1,5 +1,5 @@
 """
-Script to make a .zip file into a .cbz file (essentially renaming)
+Script to make a .zip or .rar file into a .cbz file (essentially renaming)
 
 Needs an 'input' folder in the same dir. as the script for input
 Will output into a folder called 'output'
@@ -18,6 +18,12 @@ def rename_and_move_zip_to_cbz(input_directory, export_directory):
     for filename in os.listdir(input_directory):
         if filename.endswith('.zip'):
             new_filename = filename.replace('.zip', '.cbz')
+            old_file = os.path.join(input_directory, filename)
+            new_file = os.path.join(export_directory, new_filename)
+            os.rename(old_file, new_file)
+            print(f"Moved and renamed {filename} to {new_filename} in the export directory")
+        elif filename.endswith('.rar'):
+            new_filename = filename.replace('.rar', '.cbr')
             old_file = os.path.join(input_directory, filename)
             new_file = os.path.join(export_directory, new_filename)
             os.rename(old_file, new_file)
